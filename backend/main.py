100cf7996ac8c2e0efabfde50e1dd7a3bd495578
Doğukan Ürker <dogukanurker@icloud.com>
2023-08-25 01:12:33 +0300

    server port changed to "1202"

--- backend/main.py
+++ backend/main.py
@@ -23,4 +23,4 @@ def checkURL(url: urlModel):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, workers=1, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=1202, workers=1, reload=True)
